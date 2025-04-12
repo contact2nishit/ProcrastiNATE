921d6566060354ba190b4c8e41756f199f54edff
Sai Sudarshan Barath <saisudarshanb@gmail.com>
2025-04-12 14:43:24 -0500

    start registration/login

--- src/app.py
+++ src/app.py
@@ -1,9 +1,37 @@
-from typing import Union
-
-from fastapi import FastAPI
+from db_interactions import connect, RegistrationDataModel, Token, TokenData, User, UserInDB
+from datetime import datetime, timedelta, timezone
+from typing import Annotated, Union
+import jwt
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jwt.exceptions import InvalidTokenError
+from passlib.context import CryptContext
+from pydantic import BaseModel
 
 app = FastAPI()
 
+@app.post("/register")
+async def register(data: RegistrationDataModel, status_code=status.HTTP_201_CREATED):
+    if not data.username or not data.email or not data.pwd:
+        status_code = status.HTTP_400_BAD_REQUEST
+        return {"error": "Username, email, or password missing"}
+    user = data.username
+    mail = data.email
+    pwd = data.pwd
+    with connect() as conn:
+        with conn.cursor() as curs: 
+                curs.execute("SELECT id FROM users WHERE username = %s OR email = %s", (username, email))
+                user = curs.fetchone()
+                if user: 
+                    status_code = status.HTTP_409_CONFLICT
+                    return {"error": "already exists"}
+                hash = hashpw(password.encode('utf-8'), gensalt()).decode('utf-8')
+                curs.execute("INSERT INTO users(username, email, password_hash) VALUES(%s, %s, %s)", (username, email, hash))
+                conn.commit()
+                return jsonify({"message": "user added"}), 201
+    except Exception:
+        return jsonify({"message": "something is wrong"}), 500
+        
 
 @app.get("/")
 def read_root():
---
+++ src/db_interactions.py
@@ -0,0 +1,30 @@
+import dotenv
+import os
+import psycopg
+from pydantic import BaseModel
+
+class RegistrationDataModel(BaseModel):
+    username: str
+    email: str
+    pwd: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str | None = None
+
+
+class User(BaseModel):
+    username: str
+    email: str | None = None
+
+class UserInDB(User):
+    hashed_password: str
+
+def connect():
+    dotenv.load_dotenv()
+    return psycopg.connect(f'dbname={os.getenv("DB_NAME")} user={os.getenv("DB_USER")} password={os.getenv("DB_PASSWORD")} host={os.getenv("DB_HOST")} port={os.getenv("DB_PORT")}')
+
