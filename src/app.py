e4718f52f62b365f2b1a8d3315e1c584b1710bcf
contact2nishit <contact2nishit@gmail.com>
2025-05-28 11:59:29 -0500

    almost completed fetch

--- src/app.py
+++ src/app.py
@@ -102,7 +102,7 @@ async def schedule(sched: ScheduleRequest, token: Annotated[str, Depends(oauth2_
     # TODO: insert link_or_loc if not None
     # TODO: clean up confusing naming
     try:
-        user = await get_current_user(token.strip(), app.state.pool)
+        user = await get_current_user(token, app.state.pool)
         schedules = schedule_tasks(sched.meetings, sched.assignments, sched.chores) # 3 schedules for now
         # for each meeting
             # create a meeting in response object in parallel list
@@ -113,7 +113,7 @@ async def schedule(sched: ScheduleRequest, token: Annotated[str, Depends(oauth2_
                 meeting_id1: int = await conn.fetchval("INSERT INTO meetings(user_id, meeting_name, recurs) VALUES($1, $2, $3) RETURNING meeting_id", user.user_id, meeting.name, recurs)
                 occurence_ids = []
                 for times in meeting.start_end_times:
-                    occurence_id: int = await conn.fetchval("INSERT INTO meeting_occurences(user_id, meeting_id, start_time, end_time) VALUES($1, $2, $3, $4) RETURNING occurrence_id", user.user_id, meeting_id1, times[0], times[1])
+                    occurence_id: int = await conn.fetchval("INSERT INTO meeting_occurences(user_id, meeting_id, start_time, end_time) VALUES($1, $2, $3, $4) RETURNING occurence_id", user.user_id, meeting_id1, times[0], times[1])
                     occurence_ids += [occurence_id]
                 meeting_response = MeetingInResponse(ocurrence_ids=occurence_ids, meeting_id=meeting_id1, name=meeting.name, start_end_times=meeting.start_end_times, link_or_loc=meeting.link_or_loc)
                 meeting_resp += [meeting_response]
@@ -163,25 +163,77 @@ async def fetch(start_time: str, end_time: str, meetings: bool, assignments: boo
     """
     try:
         async with app.state.pool.acquire() as conn:
+            user = await get_current_user(token, app.state.pool)
             start_time = datetime.fromisoformat(start_time)
             end_time = datetime.fromisoformat(end_time)
             if start_time > end_time:
                 raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Start time must be before end time")
             if meetings:
-                meetings = await conn.fetch("SELECT meeting_id, occurrence_id FROM meeting_occurences WHERE start_time>$1 AND end_time<$2", start_time, end_time)
+                meetings = await conn.fetch("""
+                    SELECT mo.occurence_id, mo.meeting_id, mo.start_time, mo.end_time, m.meeting_name, m.location_or_link
+                    FROM meeting_occurences mo
+                    JOIN meetings m ON mo.meeting_id = m.meeting_id
+                    WHERE mo.user_id = $1 AND mo.end_time > $2 AND mo.start_time < $3
+                """, user.user_id, start_time, end_time)
+                print(start_time, end_time, user.user_id)
+                print(type(start_time))
+                #meetings = await conn.fetch("SELECT meeting_id, occurence_id FROM meeting_occurences WHERE start_time>$1 AND end_time<$2", start_time, end_time)
             else:
                 meetings = []
             if assignments:
-                assignments = await conn.fetch("SELECT assignment_id, occurrence_id FROM assignment_occurences WHERE start_time>$1 AND end_time<$2", start_time, end_time)
+                assignments = await conn.fetch("""
+                    SELECT ao.occurence_id, ao.assignment_id, ao.start_time, ao.end_time, a.assignment_name, a.effort, a.deadline
+                    FROM assignment_occurences ao
+                    JOIN assignments a ON ao.assignment_id = a.assignment_id
+                    WHERE ao.user_id = $1 AND ao.end_time > $2 AND ao.start_time < $3
+                """, user.user_id, start_time, end_time)
             else:
                 assignments = []
             if chores:
-                print("fetching chores")
-                chores = await conn.fetch("SELECT chore_id, occurrence_id FROM chore_occurences WHERE start_time>$1 AND end_time<$2", start_time, end_time)
+                chores = await conn.fetch("""
+                SELECT co.occurence_id, co.chore_id, co.start_time, co.end_time, c.chore_name, c.effort, c.start_window, c.end_window
+                FROM chore_occurences co
+                JOIN chores c ON co.chore_id = c.chore_id
+                WHERE co.user_id = $1 AND co.end_time > $2 AND co.start_time < $3
+                """, user.user_id, start_time, end_time)
             else:
                 chores = []
             print(meetings, assignments, chores)
+            meetings_responses, assignments_responses, chore_responses = [], [], []
+            for row in meetings:
+                meeting_response = MeetingInResponse(
+                    occurence_ids=[row['occurence_id']],
+                    meetings_id=row['meeting_id'],
+                    name = row.meeting_name,
+                    start_end_times=[[row.start_time, row.end_time]],
+                    link_or_loc = row.link_or_loc
+                )
+                meetings_responses.append(meeting_response)
+            for row in assignments:
+                assignment_response = AssignmentInResponse(
+                    occurence_ids=[row['occurence_id']],
+                    assignment_id=row['assignment_id'],
+                    name = row.assignment_name,
+                    start_end_times=[[row.start_time, row.end_time]],
+                    effort = row.effort,
+                    due = row.deadline,
+                    # schedule=ScheduledTaskInfo(
+                    #     effort_assigned = 
+                    # )
+                )
+                assignments_responses.append(assignment_response)
+            for row in chores:
+                chore_response = ChoreInResponse(
+                    occurence_ids=[row['occurence_id']],
+                    chore_id=row['chore_id'],
+                    name = row.chore_name,
+                    start_end_times = [[row.start_time, row.end_time]],
+                    effort = row.effort,
+                    window = [row.start_window, row.end_window]
+                )
             return FetchResponse(meetings=meetings, assignments=assignments, chores=chores)
+    except HTTPException as e:
+        raise e
     except Exception as e:
         print(e)
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal server error")
