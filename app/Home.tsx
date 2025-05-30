ca102580ab334e4fb6eca530492a4cab5e38ba98
exa2022 <ekansh.ddn@gmail.com>
2025-05-30 16:50:28 -0500

    changes

--- app/Home.tsx
+++ app/Home.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity, Modal, StyleSheet, Alert } from 'react-native';
+import { View, Text, TouchableOpacity, Modal, StyleSheet, Alert, ScrollView } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useNavigation } from 'expo-router';
 import { useRoute } from '@react-navigation/native';
