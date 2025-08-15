d093105ac189f9cf7b0fe04b0510f79f174ec7f8
contact2nishit <contact2nishit@gmail.com>
2025-08-15 13:37:43 -0500

    fixed CORS

--- src/app.py
+++ src/app.py
@@ -48,7 +48,7 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://end-procrastination.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["authorization", "content-type"],
